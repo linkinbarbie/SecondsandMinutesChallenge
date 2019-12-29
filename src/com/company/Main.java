c6768a8387873cddbec0fe94f5b86ba7bda5fac3
linkinbarbie <linkinbarbie@gmail.com>
2019-12-29 14:04:03 +0000

    Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,40 @@
 package com.company;
 
 public class Main {
+    //this is a constant in which the variable cannot be changed.
+    //constants are usually typed in upper case so that they cannot be changed
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
 
     public static void main(String[] arg
     ) {
-        getDurationString(800, 59);
-        getDurationString(7559);
+        System.out.println(getDurationString( 800, 53));
+        System.out.println(getDurationString(7559));
+        System.out.println(getDurationString(9));
+        System.out.println(getDurationString(65,9));
     }
 
-    public static int getDurationString(int minutes, int seconds) {
+
+
+
+
+    public static String getDurationString(int minutes, int seconds) {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
-            System.out.println("Invalid Value");
-            return -1;
+            return INVALID_VALUE_MESSAGE ;
+
         } else {  //double secondsCheck = (double) seconds/60;
             int ftAnswer = minutes / 60;
 
             int remainingMinutes = minutes % 60;
 
-            System.out.println(ftAnswer + "h " + remainingMinutes + "m " + seconds + "s ");
-            return ftAnswer;
+
+            return ftAnswer + "h " + remainingMinutes + "m " + seconds + "s ";
 
         }
     }
 
-    public static int getDurationString(int seconds) {
+    public static String getDurationString(int seconds) {
         if (seconds < 0) {
-            System.out.println("Invalid value");
-            return -1;
+            return INVALID_VALUE_MESSAGE;
 
         } else {
             int newMinutes = seconds / 60;
